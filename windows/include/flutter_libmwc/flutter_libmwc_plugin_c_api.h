8ef552711a3c6afc631078b7106c1d6c762b5635
sneurlax <sneurlax@gmail.com>
2025-10-15 13:09:02 -0500

    fix(windows): fix typo in header

--- windows/include/flutter_libmwc/flutter_libmwc_plugin_c_api.h
+++ windows/include/flutter_libmwc/flutter_libmwc_plugin_c_api.h
@@ -13,7 +13,7 @@
 extern "C" {
 #endif
 
-FLUTTER_PLUGIN_EXPORT void FlutterLibMWCPluginCApiRegisterWithRegistrar(
+FLUTTER_PLUGIN_EXPORT void FlutterLibmwcPluginCApiRegisterWithRegistrar(
     FlutterDesktopPluginRegistrarRef registrar);
 
 #if defined(__cplusplus)
