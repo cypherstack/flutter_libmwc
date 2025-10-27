941a23101492c3316cd5e896615373762b4235f1
Julian <julian@cypherstack.com>
2025-10-27 11:16:52 -0600

    chore(macos build): tweak build script

--- scripts/macos/build_all.sh
+++ scripts/macos/build_all.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-rm -r build
+set -e
+rm -rf build
 mkdir build
 echo ''$(git log -1 --pretty=format:"%H")' '$(date) >> build/git_commit_version.txt
 VERSIONS_FILE=../../lib/git_versions.dart
@@ -9,7 +10,7 @@ if [ ! -f "$VERSIONS_FILE" ]; then
 fi
 COMMIT=$(git log -1 --pretty=format:"%H")
 OSX="OSX"
-sed -i '' "/\/\*${OS}_VERSION/c\\/\*${OS}_VERSION\*\/ const ${OS}_VERSION = \"$COMMIT\";" $VERSIONS_FILE
+sed -i '' '/\/\*${OS}_VERSION/c\'$'\n''/\*${OS}_VERSION\*\/ const ${OS}_VERSION = "'"$COMMIT"'";' "$VERSIONS_FILE"
 cp -r ../../rust build/rust
 cd build/rust
 
